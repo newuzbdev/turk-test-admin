95194a5fd4a1df2410d932b0d26ba8399c8fb008
Muhammad4302001 <muhammad4302001@gmail.com>
2025-07-29 16:29:39 +0500

    fix restore error

--- src/config/queries/archive/restore.queries.ts
+++ src/config/queries/archive/restore.queries.ts
@@ -2,15 +2,15 @@ import { useMutation, useQueryClient } from "@tanstack/react-query";
 import toast from "react-hot-toast";
 import axiosPrivate from "@/config/api";
 import { testEndpoints } from "@/config/endpoint";
-import type { ApiResponse, Test } from "@/utils/types/types";
+import type { ApiResponse, OnlyTest } from "@/utils/types/types";
 
 export const useRestoreTest = () => {
   const queryClient = useQueryClient();
   return useMutation({
     mutationFn: async (id: string) => {
       return (
-        await axiosPrivate.patch<ApiResponse<Test>>(
-          testEndpoints.one(id),
+        await axiosPrivate.patch<ApiResponse<OnlyTest>>(
+          testEndpoints.onlyOne(id),
           { isDeleted: false }
         )
       ).data;
@@ -18,6 +18,7 @@ export const useRestoreTest = () => {
     onSuccess: () => {
       // Invalidate both archive and main test queries
       queryClient.invalidateQueries({ queryKey: [testEndpoints.all] });
+      queryClient.invalidateQueries({ queryKey: [testEndpoints.only] });
       toast.success("Test muvaffaqiyatli qayta tiklandi");
     },
     onError: () => {
